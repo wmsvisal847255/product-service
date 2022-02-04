fe1bc95faad49dcd9e350d7f34fc2d5317e50142
hashaneAponso <hashaneaponso@gmail.com>
2022-02-05 00:57:24 +0530

    change table name

--- src/handlers/save-update-product.js
+++ src/handlers/save-update-product.js
@@ -19,7 +19,7 @@ exports.handler = async(event) => {
     console.error(product);
     console.error(tableName);
     let data = await dynamodb.put({
-        TableName: ProductTripDev,
+        TableName: 'ProductTripDev',
         Item: product
     }).promise();
     
